f8a3261f2f6cf1d9308776f3eda2badabc2c623e
sathyapriya-t <sathya2001t@gmail.com>
2022-10-27 18:19:32 +0530

    Initial commit

---
+++ src/main/java/com/employeeproject/springbootfirstapp/employeeFirstAppApplication.java
@@ -0,0 +1,14 @@
+package com.employeeproject.springbootfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class employeeFirstAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(employeeFirstAppApplication.class, args);
+	}
+
+}
