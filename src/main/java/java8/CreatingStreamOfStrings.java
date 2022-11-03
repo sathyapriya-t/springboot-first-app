93795dc044f4f4d4e655effd8c2b645068ce9a18
sathyapriya-t <sathya2001t@gmail.com>
2022-11-03 08:16:26 +0530

    java 8 stream feature - Creating stream of strings

--- src/main/java/java8/CreatingStreamOfStrings.java
+++ src/main/java/java8/CreatingStreamOfStrings.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class CreatingStreamOfStrings {
     public static void main(String[] args) {
         List<String> listOfStrings = Arrays.asList("hi","hello","world");
-
         listOfStrings.stream().forEach(System.out::println);
 
 
