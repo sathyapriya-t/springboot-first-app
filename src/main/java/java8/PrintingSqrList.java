37c8eb8f51bea6b8a255e1e9ca44af21110f27ff
sathyapriya-t <sathya2001t@gmail.com>
2022-11-03 08:14:02 +0530

    java 8 stream feature - Printing square of the numbers in the list.

--- src/main/java/java8/PrintingSqrList.java
+++ src/main/java/java8/PrintingSqrList.java
@@ -2,12 +2,12 @@ package java8;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class PrintingSqrList {
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
-        List<Integer> squareList = integerList.stream().map(x->x*x).collect(Collectors.toList());
+        List<Integer> squareList = integerList.stream().map(x->x*x).toList();
         squareList.forEach(System.out::println);
     }
 }
