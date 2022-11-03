19fb263411b3ab944035a5d68bbb3447015280e1
sathyapriya-t <sathya2001t@gmail.com>
2022-11-03 07:56:02 +0530

    Dependency injection coding

---
+++ src/main/java/java8/CreatingStreamOfStrings.java
@@ -0,0 +1,13 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CreatingStreamOfStrings {
+    public static void main(String[] args) {
+        List<String> listOfStrings = Arrays.asList("hi","hello","world");
+        listOfStrings.stream().forEach(System.out::println);
+
+
+    }
+}
